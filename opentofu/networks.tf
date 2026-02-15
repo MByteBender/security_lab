8366d32804873bc4b06ced8850edb073f1a2f63c
patrick <thordal.simetzberger@gmail.com>
2026-02-15 18:15:04 +0100

    tofu network configuration

---
+++ opentofu/networks.tf
@@ -0,0 +1,49 @@
+# LAN network
+resource "proxmox_virtual_environment_network_linux_bridge" "vlan_bridge" {
+  node_name = "pve"    # Your Proxmox node name
+  name      = "Intern"  # The name of the new bridge
+
+  # The IPv4 address for the Proxmox host on this bridge
+  address   = "10.0.10.0/24"
+
+  # Enables VLAN tagging for VMs
+  vlan_aware = true
+
+  comment = "General VLAN Bridge managed by OpenTofu"
+}
+
+# DMZ network
+resource "proxmox_virtual_environment_network_linux_bridge" "vlan_bridge" {
+  node_name = "pve"    # Your Proxmox node name
+  name      = "DMZ"  # The name of the new bridge
+
+  # The IPv4 address for the Proxmox host on this bridge
+  address   = "10.0.20.0/24"
+
+  # Enables VLAN tagging for VMs
+  vlan_aware = true
+
+  comment = "General VLAN Bridge managed by OpenTofu"
+}
+
+# fake Internet network
+resource "proxmox_virtual_environment_network_linux_bridge" "vlan_bridge" {
+  node_name = "pve"    # Your Proxmox node name
+  name      = "Extern"  # The name of the new bridge
+
+  # The IPv4 address for the Proxmox host on this bridge
+  address   = "10.0.0.0/24"
+
+  comment = "General VLAN Bridge managed by OpenTofu"
+}
+
+# Management network
+resource "proxmox_virtual_environment_network_linux_bridge" "vlan_bridge" {
+  node_name = "pve"    # Your Proxmox node name
+  name      = "Management"  # The name of the new bridge
+
+  # The IPv4 address for the Proxmox host on this bridge
+  address   = "192.168.0.0/24"
+
+  comment = "General VLAN Bridge managed by OpenTofu"
+}
