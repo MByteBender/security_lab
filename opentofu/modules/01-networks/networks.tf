56a6fecde62e934d35e5e69833aecd28d4aa8315
patrick <thordal.simetzberger@gmail.com>
2026-03-18 03:44:13 +0100

    redo structure and adding main tofu file

--- opentofu/modules/01-networks/networks.tf
+++ opentofu/modules/01-networks/networks.tf
@@ -19,14 +19,36 @@ variable "pve_node_name" {
 }
 
 # --- 2. Create the 4 Isolated Bridges ---
-resource "proxmox_virtual_environment_network_linux_bridge" "isolated_nets" {
-  for_each = toset(["110", "120", "130", "140", "1255"])
-
+resource "proxmox_virtual_environment_network_linux_bridge" "lan" {
   node_name = var.pve_node_name
-  name      = "vmbr${each.key}"
-  comment   = "Tofu-Isolated-Net-${each.key}"
-
+  name      = "vmbr110"
+  comment   = "Tofu-Isolated-Net-110"
 }
+
+resource "proxmox_virtual_environment_network_linux_bridge" "dmz" {
+  node_name = var.pve_node_name
+  name      = "vmbr120"
+  comment   = "Tofu-Isolated-Net-120"
+}
+
+resource "proxmox_virtual_environment_network_linux_bridge" "extern" {
+  node_name = var.pve_node_name
+  name      = "vmbr130"
+  comment   = "Tofu-Isolated-Net-130"
+}
+
+resource "proxmox_virtual_environment_network_linux_bridge" "setup" {
+  node_name = var.pve_node_name
+  name      = "vmbr140"
+  comment   = "Tofu-Isolated-Net-140"
+}
+
+resource "proxmox_virtual_environment_network_linux_bridge" "management" {
+  node_name = var.pve_node_name
+  name      = "vmbr1255"
+  comment   = "Tofu-Isolated-Net-1255"
+}
+
 
 # --- 3. The "Token-Based" Apply (The Workaround) ---
 # Since the provider resource is missing, we use curl to hit the API 'Apply' endpoint.
