0013fd32c4157bd719779b5b0996f54961ba10fd
patrick <thordal.simetzberger@gmail.com>
2026-03-18 03:38:12 +0100

    redo structure and adding main tofu file

--- opentofu/modules/01-networks/networks.tf
+++ opentofu/modules/01-networks/networks.tf
@@ -40,7 +40,7 @@ resource "null_resource" "apply_network_via_api" {
 
   provisioner "local-exec" {
     command = <<EOT
-      sleep 5 && curl -X POST "${var.proxmox_api_url}/nodes/${var.pve_node_name}/network" \
+      sleep 20 && curl -X POST "${var.proxmox_api_url}/nodes/${var.pve_node_name}/network" \
         -H "Authorization: PVEAPIToken=${var.proxmox_api_token}" \
         -k
     EOT
