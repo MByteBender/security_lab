b8cf0cc3fa54b34e9afb14e08a965ac4f284de4e
patrick <thordal.simetzberger@gmail.com>
2026-03-21 01:19:06 +0100

    management

--- opentofu/fromScratch.sh
+++ opentofu/fromScratch.sh
@@ -1,7 +1,12 @@
+# 
+rm -rf .terraform .terraform terraform.tfstate terraform.tfstate.backup
+
+./packer_built_all.sh
 
 # initialize tofu
 tofu init
 
+tofu import -var-file="credentials.tfvars" 'module.management.proxmox_virtual_environment_vm.management' pve/100
+
 # create network
-tofu plan -var-file="credentials.tfvars"
 tofu apply -var-file="credentials.tfvars"
