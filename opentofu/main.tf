8dc80f9abcd8546fda79f4570de3532182419680
patrick <thordal.simetzberger@gmail.com>
2026-03-29 01:00:16 +0100

    management

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -16,6 +16,11 @@ variable "proxmox_api_token" {
   sensitive = true
 }
 
+variable "ubuntu_password_plain" {
+  type    = string
+  sensitive = true
+}
+
 provider "proxmox" {
   endpoint  = var.proxmox_api_url
   api_token = var.proxmox_api_token
