c8f660f6a03d38c7a3666a9d059637f1fc1681aa
thordal <thordal@simetzberger.at>
2026-04-08 20:29:08 +0200

    management

--- opentofu/modules/06-ubuntu-desktop/ubuntu_desktop.tf
+++ opentofu/modules/06-ubuntu-desktop/ubuntu_desktop.tf
@@ -123,7 +123,7 @@ iface $INTERFACE inet static
 #EOF
 
       # 4. Restart the legacy networking service
-    sudo sh -c 'nohup sh -c \"/etc/init.d/networking restart && sleep 10\" > /dev/null 2>&1 &'
+    nohup sh -c \"/etc/init.d/networking restart && sleep 10\"
     EOT
   ]
 }
