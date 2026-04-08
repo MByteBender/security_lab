dba082de1d8e13b25fa10f9d0ab79481230044ac
patrick <thordal.simetzberger@gmail.com>
2026-04-08 15:52:59 +0200

    management

--- opentofu/modules/07-win7/win7.tf
+++ opentofu/modules/07-win7/win7.tf
@@ -51,14 +51,15 @@ resource "proxmox_virtual_environment_vm" "win7" {
   }
 
   network_device {
-    bridge = "vmbr110"
-    mac_address = "AA:11:00:15:00:00"
+    bridge = "vmbr140"
+    mac_address = "AA:14:00:15:00:00"
     model  = "e1000"
   }
 
+
   network_device {
-    bridge = "vmbr140"
-    mac_address = "AA:14:00:15:00:00"
+    bridge = "vmbr110"
+    mac_address = "AA:11:00:15:00:00"
     model  = "e1000"
   }
 
