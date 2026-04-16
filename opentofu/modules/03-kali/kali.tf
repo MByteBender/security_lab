e99f534a4de45f8e117f3bcb7fe74fc9049521d7
patrick <thordal.simetzberger@gmail.com>
2026-04-16 20:52:03 +0200

    management

--- opentofu/modules/03-kali/kali.tf
+++ opentofu/modules/03-kali/kali.tf
@@ -89,7 +89,6 @@ resource "proxmox_virtual_environment_vm" "kali" {
   }
 
 provisioner "remote-exec" {
-    on_failure = continue
     inline = [
         <<-EOT
           INTERFACE2=$(ip -o link show | grep -i 'AA:14:00:11:00:00' | awk -F': ' '{print $2}')
