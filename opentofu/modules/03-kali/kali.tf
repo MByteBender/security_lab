a6ebb9a8ae32494573e74eaa52ee76ce4c207b13
patrick <thordal.simetzberger@gmail.com>
2026-04-18 01:37:44 +0200

    management

--- opentofu/modules/03-kali/kali.tf
+++ opentofu/modules/03-kali/kali.tf
@@ -119,8 +119,7 @@ EOF"
 
 echo "${var.kali_password}" | sudo -S bash -c "(sleep 2; systemctl restart networking) > /dev/null 2>&1 &"
           echo "Network restart triggered. Connection will drop, but config is applied."
-          exit 0
-ip a
+ip a && sleep 2
       EOT
     ]
   }
