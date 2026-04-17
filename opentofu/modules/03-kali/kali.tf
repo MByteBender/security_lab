71f04b3820d41878c7d0c7404cb08ada4ad82e6f
patrick <thordal.simetzberger@gmail.com>
2026-04-18 00:09:25 +0200

    management

--- opentofu/modules/03-kali/kali.tf
+++ opentofu/modules/03-kali/kali.tf
@@ -104,13 +104,13 @@ provisioner "remote-exec" {
 
 echo "${var.kali_password}" | sudo -S bash -c "cat <<EOF | tee /etc/network/interfaces.d/setup
 auto $INTERFACE
-iface $INTERFACE inet static
-    address 10.0.30.110/24
-    link-module none
-    post-up ip addr flush dev $INTERFACE
-    post-up ip route add 10.0.10.0/24 via 10.0.30.1 dev $INTERFACE
-    post-up ip route add 10.0.20.0/24 via 10.0.30.1 dev $INTERFACE
-
+iface $INTERFACE inet manual
+    up ip link set $IFACE up
+    up ip addr add 10.0.30.110/24 dev $IFACE
+    up ip route add 10.0.10.0/24 via 10.0.30.1 dev $IFACE
+    down ip addr flush dev $IFACE
+    down ip link set $IFACE down
+    
 auto $INTERFACE2
 iface $INTERFACE2 inet static
     address 10.0.40.110/24
