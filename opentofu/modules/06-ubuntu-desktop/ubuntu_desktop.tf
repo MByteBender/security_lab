336c2d73bc7d3fb5eaa7436efe36b6932a817725
patrick <thordal.simetzberger@gmail.com>
2026-04-19 01:13:22 +0200

    management

--- opentofu/modules/06-ubuntu-desktop/ubuntu_desktop.tf
+++ opentofu/modules/06-ubuntu-desktop/ubuntu_desktop.tf
@@ -125,7 +125,7 @@ iface $INTERFACE2 inet static
     gateway 10.0.40.1
 EOF
 
-    echo "ubuntu" | sudo -S /etc/init.d/networking restart && sleep 10
+    echo "ubuntu" | sudo -S /etc/init.d/networking restart && sleep 20
     ip a && sleep 2
     EOT
   ]
